8eba61c4d430adc198ffdeadda1e860cfffa1a5b
MaksymSirenko <brute1988@gmail.com>
2024-04-12 08:47:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+// ?console.log('welcome js')
+
+let x
+x = 'word'
+x = ' another word'
+
+// ?console.log(x);
+
+const fruit = 'orange'
+
+x = 42
+//? console.log(x);
+
+//*Stroka
+let planet = "Earth"
+planet = 'Mercury'
+planet = `Mars`
+
+//? console.log(planet)
+
+//* Zahlen
+
+let n1 = 9
+let n2 = 8.5
+//?console.log(Number.MAX_SAFE_INTEGER)
+
+
+//* Bigint
+
+let n3 = 9n
+
+console.log(typeof n3)
+
+//* Boolean
+
+let isDrink = false
+let hasMother = true
+
+//* Undefined
+
+let u1 = undefined
+let u2
+console.log(u2)
+
+console.log(typeof u1)
+
+//* Null
+
+let null1 = null
+console.log(null1)
+//*symbol
+let s1 = Symbol('smth')
+console.log(s1)
+
+let result = 10 / 'привет'
+console.log(result)
+console.log(typeof result)
+
+console.log(1 + 1)
+
+console.log('1' + 1)
+
+//*1 В строку
+
+let s2 = 5 + " "
+console.log(s2, typeof s2)
+
+let s3 = String(false)
+console.log(typeof s3)
+
+let s4 = +'42'
+console.log(typeof s4)
+
+let s5 = Number('777')
+console.log(typeof s5)
+
+let s6 = parseInt('777fdfd76')
+console.log(s6)
+
+let s7 = 5 + " "
+console.log(s7, typeof s7)
+
+let l5 = Boolean(0)
+console.log(l5)
+let l6 = Boolean(NaN)
+console.log(l6)
+let l7 = Boolean(-8)
+console.log(l7)
+let l8 = Boolean(6346)
+console.log(l8)
+
+
+let l9 = 8 / 2
+let l10 = 8 * 0
+let l11 = 8 + 0
+let l12 = 8 - 0
+let l13 = 8 % 3
+let l14 = 8 ** 8
+console.log(l9, l10, l11, l12, l13, l14)
+
+
+console.log(9 == '9')
+console.log(9 === '9')
+
+console.log(9 !== '9')
+
+console.log()
+
+console.log('99' + 9 -5)
+console.log((2+2) ** 3)
