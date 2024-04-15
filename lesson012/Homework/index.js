13efa7ffdbd787c2bf82e881624d382c1a70f567
MaksymSirenko <brute1988@gmail.com>
2024-04-15 23:21:05 +0200

    zwanwigsten commit

---
+++ lesson012/Homework/index.js
@@ -0,0 +1,69 @@
+// ## Задание 1
+// Создайте стрелочную функцию, которая бы принимала массив и цифру, возвращала бы элемент массива под этим индексом.
+
+const neuArray = ["null", "eins", "zwei", "drei"]
+const unserFunction = (array, number) => {
+    if (array[number] == undefined) {
+        return "No such index in array"
+    } else {
+        return array[number]
+    }
+
+}
+
+console.log(unserFunction(neuArray, 5))
+
+console.log("==================================")
+
+
+const wettFunction = (code) => {
+    switch (code) {
+        case 'SQ':
+            return "шквал";
+        case 'PO':
+            return "пыльный вихрь";
+        case 'FC':
+            return "торнадо";
+        case 'BR':
+            return "дымка (видимость от 1 до 9 км)";
+        case 'HZ':
+            return "мгла (видимость менее 10 км)";
+        case 'FU':
+            return "дым (видимость менее 10 км)";
+        case 'DS':
+            return "пыльная буря (видимость менее 10 км)";
+        case 'SS':
+            return "песчаная буря (видимость менее 10 км)";
+        default:
+            return "Не корректный код";
+    }
+}
+
+console.log(wettFunction('BRD'))
+
+const wettAntwrort = function (code, wettFunction) {
+    return wettFunction(code)
+}
+
+console.log(wettAntwrort('SS', wettFunction))
+
+
+console.log('=================================')
+
+// Задание 3
+// Напишите функцию sumNumbers, которая принимает на вход массив чисел и возвращает строку, в которой перечислены все числа из массива и их сумма. Используйте синтаксис function expression, цикл while, шаблонные строки.
+// console.log(sumNumbers(numbers)); // Ожидаемый результат: "Числа: 1, 2, 3, 4, 5. Сумма: 15."
+
+const myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let sum =0
+const sumNumbers = (array) => {
+    let i=0
+    while (i < array.length){
+        sum = sum+array[i]
+        i++
+    }
+    return `Ожидаемый результат: "Числа: ${array}. Сумма: ${sum}."`
+}
+
+console.log(sumNumbers(myArray))
---
+++ lesson012/index.js
@@ -0,0 +1,40 @@
+
+
+
+const helloEng = function(firstName, lastName){
+    return 'Good day, '+ firstName +' '+ lastName
+}
+
+
+console.log(helloEng('Bilbo','Baggins'))
+
+const helloTurkish = function(firstName, lastName){
+    return 'Merhaba, ' + firstName +' '+ lastName
+}
+
+console.log(helloTurkish('Bilbo','Baggins'))
+
+
+greetingFunction = function(firstName, lastName){
+return firstName + ' '+lastName
+  
+}
+const universalHello = function(firstName, lastName, greetingFunction){
+greetingFunction(firstName,lastName)
+}
+
+
+
+console.log(universalHello ('Bilbo','Baggins'))
+
+
+
+
+
+
+
+
+
+
+
+
